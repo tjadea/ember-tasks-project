59800aa4d7abce7ea9f9878441027d502af60d9b
Tamara Argent <tjargent@outlook.com>
2018-03-22 19:44:22 +0000

    Removed irrelevant files.

--- app/router.js
+++ app/router.js
@@ -7,8 +7,6 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
-  this.route('services');
-  this.route('actions');
   this.route('tasks', function() {
     this.route('new');
     this.route('edit', {path: '/edit/:task_id'});
