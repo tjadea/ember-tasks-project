9c62fad19820beb4691529008bede42a38990960
Tamara Argent <tjargent@outlook.com>
2018-03-22 19:51:42 +0000

    Fix navbar menu and Bootstrap JS support for hamburger/toggle menu.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,6 +21,7 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
 
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  app.import('bower_components/bootstrap/dist/js/bootstrap.min.js');
   app.import('bower_components/moment/min/moment.min.js');
   return app.toTree();
 };
